8c670d3b8bf897a9478d5a0b2c8caddf2dd85d37
MQW <qianwenma915@163.com>
2017-02-28 13:27:35 +0800

    fix bug

--- test.py
+++ test.py
@@ -2,13 +2,12 @@ from living_search.spiders.bilibili import *
 from living_search.dbHelper import *
 from scrapy.crawler import CrawlerRunner,reactor
 from scrapy.settings import Settings
-from scrapy.utils.project import get_project_settings  # 导入seetings配置
+
 # db = DBHelper()
 # sql = "select a.sample_topicsID as topics_id,a.sample_keyword as word,a.t_rule_id as rule_id  from sample_keywords a order by sample_topicsID desc"
 # params = ()
 # data = db.find(sql, *params)
 settings = Settings()
-
 # crawl settings
 settings.set("USER_AGENT", "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36")
 settings.set("ITEM_PIPELINES" , {
